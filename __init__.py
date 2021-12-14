aadd3377e5f9fa94736db73ac8a118ff0e43efb4
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2021-12-14 09:29:57 +0100

    added intent_handler

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
 
 
 class Kalender(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('kalender.intent')
+    @intent_handler('kalender.intent')
     def handle_kalender(self, message):
         self.speak_dialog('kalender')
 
