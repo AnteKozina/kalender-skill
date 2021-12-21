1524a3fd1c9af24de6d69e2bb08177bac81f5979
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2021-12-21 09:42:54 +0100

    caldav changes

--- caldav_starter.py
+++ caldav_starter.py
@@ -1,20 +1,35 @@
 import caldav
 from caldav.elements import dav
 from secrets import username, passwort
+import icalendar
 
 # Caldav url
 # import secret login code from local file here
-username = my_username
-password = passwort
+username = "bw040@hdm-stuttgart.de"
+password = "beckerasano"
 
-url = "https://" + username + ":" + password + "@next.social-robot.info/remote.php/dav"
+# url = f"https://{username}:{password}@next.social-robot.info/remote.php/dav"
+url = "https://nextcloud.humanoidlab.hdm-stuttgart.de/remote.php/dav/calendars/bw040@hdm-stuttgart.de/personal/"
 
 # open connection to calendar
-client = caldav.DAVClient(url)
+client = caldav.DAVClient(url=url, username=username, password=passwort)
 principal = client.principal()
 
 # get all available calendars (for this user)
 calendars = principal.calendars()
 
 # check the calendar events and parse results..
-print(calendars)
+events = calendars[0].events()
+
+parsed_events = []
+for event in events:
+    event = icalendar.Event()
+    print(event)
+
+    '''
+    cal = icalendar.Calendar.from_ical(event.data, True)
+    url = event.url
+    print(cal[0].SUMMARY)
+    print("___")
+    
+    '''
