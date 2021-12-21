0e1b520db377eb344420092de1872d47ee8fd4e3
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2021-12-21 08:53:54 +0100

    added caldav to init py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from mycroft import MycroftSkill, intent_file_handler, intent_handler
-
+import caldav
+from caldav.elements import dav
+from secrets import my_username, passwort
 
 class Kalender(MycroftSkill):
     def __init__(self):
@@ -7,6 +9,23 @@ class Kalender(MycroftSkill):
 
     @intent_handler('kalender.intent')
     def handle_kalender(self, message):
+        # Caldav url
+        # import secret login code from local file here
+        username = my_username
+        password = passwort
+
+        url = "https://" + username + ":" + password + "@next.social-robot.info/remote.php/dav"
+
+        # open connection to calendar
+        client = caldav.DAVClient(url)
+        principal = client.principal()
+
+        # get all available calendars (for this user)
+        calendars = principal.calendars()
+
+        # check the calendar events and parse results..
+        print(calendars)
+
         self.speak_dialog('kalender')
 
 
