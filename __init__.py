307ac74d5da47658923a079f64c011b85ecd15ab
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-01-17 17:45:06 +0100

    big commit

--- __init__.py
+++ __init__.py
@@ -5,6 +5,11 @@ from mycroft import MycroftSkill, intent_file_handler, intent_handler
 class Kalender(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+    
+    def initialize(self):
+        self.register_entity_file('year.entity')
+        self.register_entity_file('month.entity')
+        self.register_entity_file('day.entity')
 
     @intent_handler('kalender.next.event.intent')
     def handle_kalender(self, message):
@@ -17,9 +22,9 @@ class Kalender(MycroftSkill):
     
     @intent_handler('kalender.events.on.day.intent')
     def handle_events_on_day(self, message):
-        month = message.data["month"]
-        day = message.data["day"]
-        year = message.data["year"]
+        month = message.data.get("month")
+        day = message.data.get("day")
+        year = message.data.get("year")
         
         self.speak_dialog(f"{month} | {day} | {year}")
 
