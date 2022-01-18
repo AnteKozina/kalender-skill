e7bcbd2e62731d4b8695c314fbc6297d426cd761
Ante Kozina <Ante.Kozina@Adesso.de>
2022-01-18 10:14:17 +0100

    added all in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,10 @@
-from . import caldav_starter
 from datetime import datetime
-from helper import get_events_on_day_string, get_next_event_string
-#from secrets import USERNAME, PASSWORT, CALENDAR_URL
 from mycroft import MycroftSkill, intent_file_handler, intent_handler
 
 USERNAME = "bw040@hdm-stuttgart.de"
 PASSWORT = "beckerasano2"
 CALENDAR_URL = "https://nextcloud.humanoidlab.hdm-stuttgart.de/remote.php/dav/calendars/bw040@hdm-stuttgart.de/personal/"
+Utc = pytz.UTC
 
 
 class Kalender(MycroftSkill):
@@ -65,3 +63,138 @@ def check_year(year):
         return False
     return True
 
+
+class CalendarFunctions:
+    '''Contains all functions, that are needed for our calender skill'''
+
+    calendar = None
+
+    def __init__(self, url, calendar_username, calendar_password):
+        self.client = caldav.DAVClient(
+            url=url,
+            username=calendar_username,
+            password=calendar_password
+        )
+        principal = self.client.principal()
+        self.calendar = principal.calendars()[0]
+
+
+    def get_all_events(self):
+        '''
+        Gets all events from the calendar
+            Parameters: None
+            Returns: All events in a list
+        '''
+        events = self.calendar.events()
+        events_to_return = []
+        for event in events:
+            cal = icalendar.Calendar.from_ical(event.data, True)
+            for vevent in cal[0].walk("vevent"):
+                events_to_return.append(get_calender_events(vevent))
+
+        return events_to_return
+
+
+    def get_next_event(self):
+        '''
+        Function to get the next event from the calendar 
+        (the event with the next start date in the future)
+            Parameters: None
+            Returns: Next event
+        '''
+        all_events = self.get_all_events()
+        earliest_event = {}
+        time_now = dt.now(tz=None)
+
+        # loop through all events, if start time earlier -> replace earlist_event with current event
+        for event in all_events:
+            date_of_current_event = event["start"]
+            if date_of_current_event > time_now:
+                # If earliest event is empty, it will be false
+                if bool(earliest_event) is False:
+                    earliest_event = event
+                else:
+                    date_of_earliest_event = earliest_event["start"]
+                    if date_of_current_event < date_of_earliest_event:
+                        earliest_event = event
+
+        return earliest_event
+
+
+    def get_all_events_of_day(self, day):
+        '''
+        Returns all events for a given day
+            Parameters: Day in datetime format
+            Returns: List of events
+        '''
+        all_events = self.get_all_events()
+        events_on_day = []
+
+        for event in all_events:
+            event_date = event["start"]
+            if (event_date.year == day.year) and (event_date.month == day.month) and (event_date.day == day.day):
+                events_on_day.append(event)
+
+        return (events_on_day, day)
+
+
+def get_calender_events(cal_event):
+    '''
+    Build a calendar JSON Object from an event object
+        Parameters: Calendar Event
+        Returns: Dictionary of Events
+    '''
+    return {
+        "summary" : str(cal_event["SUMMARY"]),
+        "start" : fix_time_object(cal_event["DTSTART"].dt),
+        "end" : fix_time_object(cal_event["DTEND"].dt),
+        #"url" : cal_event["url"]
+    }
+
+
+def fix_time_object(time):
+    '''
+    Removes the timezone information, if timeobject contains timezone
+        Parameters: One Datetime Object
+        Returns: One Datetime Object without timezone
+    '''
+    if isinstance(time, datetime.date) and not isinstance(time, datetime.datetime):
+        time = dt(time.year, time.month, time.day)
+    return time.replace(tzinfo=None)
+def get_next_event_string(event):
+    """
+    Takes in an event and returns a string containing the necessary information
+    """
+    event_name = event["summary"]
+    start_time = event["start"]
+
+    year = start_time.year
+    month = start_time.strftime("%B")
+    day = start_time.day
+
+    time = start_time.strftime("%H:%M")
+    return f"Your next appointment is on {month} {day}, {year} at {time} o'clock and is entitled {event_name}."
+
+def get_events_on_day_string(events):
+    start_time = events[1]
+    year = start_time.year
+    month = start_time.strftime("%B")
+    day = start_time.day
+    return_string = f"On {month} {day}, {year} you have the following appointments: "
+    
+    # If No events on given day
+    if len(events[0]) == 0:
+        return f"You have no appointments on {month} {day}, {year}."
+
+    # If only one event on day
+    if len(events[0]) == 1:
+        return_string = f"You have the following appointment on {month} {day}, {year}: "
+
+    for event in events[0]:
+        start_time = event["start"]
+        time = start_time.strftime("%H:%M")
+        event_name = event["summary"]
+        event_string = f" {event_name} at {time} o'clock, "
+        return_string += event_string
+
+    return return_string[:-2] + "."
