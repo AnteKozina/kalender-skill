d5507fb2f105fc14831d452d353a3dc40cabe786
Ante Kozina <Ante.Kozina@Adesso.de>
2022-01-18 15:45:15 +0100

    deleted

--- __init__.py
+++ __init__.py
@@ -36,7 +36,6 @@ class Kalender(MycroftSkill):
 
         datetime_object = datetime.strptime(month, "%B")
         month_number = datetime_object.month
-        printf"{month_number}"
 
         calendar = CalendarFunctions(CALENDAR_URL, USERNAME, PASSWORT)
 
