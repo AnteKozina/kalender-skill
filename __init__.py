4c6f7259e5240c31da0a72c4b6ac3a33790a2cc5
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-02-22 14:11:09 +0100

    2f

--- __init__.py
+++ __init__.py
@@ -170,7 +170,7 @@ def get_calender_events(cal_event):
         Parameters: Calendar Event
         Returns: Dictionary of Events
     '''
-    info(cal_event)
+    info(fix_time_object(cal_event["DTSTART"].dt))
     return {
         "summary" : str(cal_event["SUMMARY"]),
         "start" : fix_time_object(cal_event["DTSTART"].dt),
