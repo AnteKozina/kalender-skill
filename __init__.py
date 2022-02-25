4d54e78e199b3ca3c647ef37d6c7dd2146bb0c5d
Ante Kozina <Ante.Kozina@Adesso.de>
2022-02-25 15:43:56 +0100

    test

--- __init__.py
+++ __init__.py
@@ -76,11 +76,12 @@ class Kalender(MycroftSkill):
         start_time = message.data.get("start_time")
         end_time = message.data.get("end_time")
         title = message.data.get("title")
-
+        '''
         day_creation_start = datetime(year, month, day, int(start_time[:2]), int(start_time[2:]))
         day_creation_end = datetime(year, month, day, int(end_time[:2]), int(end_time[2:]))
         response = create_event(self, title, day_creation_start, day_creation_end)
-        self.speak_dialog("Created event!")
+        '''
+        self.speak_dialog(start_time + " " + end_time)
 
 ''' HELPER FUNCTIONS '''
 
