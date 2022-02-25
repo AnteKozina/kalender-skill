93992e67d96e84c37e3806381d44b4c2607128a4
Ante Kozina <Ante.Kozina@Adesso.de>
2022-02-25 15:38:30 +0100

    new appointment

--- __init__.py
+++ __init__.py
@@ -77,13 +77,13 @@ class Kalender(MycroftSkill):
         end_time = message.data.get("end_time")
         title = message.data.get("title")
         self.speak_dialog("Test")
-'''
+
             if len(start_time) > 2:
                 day_creation_start = datetime(year, month, day, int(start_time[:2]), int(start_time[2:]))
                 day_creation_end = datetime(year, month, day, int(end_time[:2]), int(end_time[2:]))
                 response = create_event(self, title, day_creation_start, day_creation_end)
-                self.speak_dialog(response)
-'''
+                self.speak_dialog("Created event!")
+
 ''' HELPER FUNCTIONS '''
 
 def create_skill():
